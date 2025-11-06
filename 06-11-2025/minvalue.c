5de8efc168418220525ddf965c06ca5895535239
sredha <sredhalakshmi99@gmail.com>
2025-11-06 16:27:46 +0530

    To find min value in an array

---
+++ 06-11-2025/minvalue.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define x 100
+int main()
+{
+	int arr[x],limit,num,val,i;
+	printf("enter the limit :");
+	scanf("%d",&limit);
+	for(i=0;i<limit;i++)	
+	{
+		printf("enter the array elements at %d:",i);
+		scanf("%d",&arr[i]);
+	}
+	for (i=0;i<limit;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	val=arr[0];
+	for (i=0;i<limit;i++)
+	{
+		if (val>arr[i])
+			val=arr[i];
+	}
+	printf("\nmin value in array is %d",val);
+	printf("\n");
+	return 0;
+	
+		
+		
+	
+}
